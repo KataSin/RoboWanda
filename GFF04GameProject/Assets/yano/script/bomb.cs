0692b0bc1a587cd9a27ef8a706e1856d3edc9b65
Yano Atsuya <ATYcreateaChiya619@gmail.com>
2017-09-20 12:41:59 +0900

    [矢野]ビルスケール変更、Breakスクリプトの変更

--- GFF04GameProject/Assets/yano/script/bomb.cs
+++ GFF04GameProject/Assets/yano/script/bomb.cs
@@ -14,13 +14,13 @@ public class bomb : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-            transform.position += transform.forward / 10f;
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-            transform.position -= transform.forward / 10f;
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-            transform.position += transform.right / 10f;
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-            transform.position -= transform.right / 10f;
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.position += transform.forward * 2;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            transform.position -= transform.forward * 2;
+        else if (Input.GetKey(KeyCode.RightArrow))
+            transform.position += transform.right * 2;
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            transform.position -= transform.right * 2;
     }
 }
