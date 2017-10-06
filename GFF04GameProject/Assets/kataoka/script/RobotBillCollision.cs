7fba5f959f93199a95cf79a42360a40ace5659bb
kataoka <ktok223@gmail.com>
2017-10-06 10:26:55 +0900

    プル用

---
+++ GFF04GameProject/Assets/kataoka/script/RobotBillCollision.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotBillCollision : MonoBehaviour
+{
+    private bool m_IsCollision;
+    // Use this for initialization
+    void Start()
+    {
+        m_IsCollision = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_IsCollision = false;
+    }
+    /// <summary>
+    /// 当たっているかどうかを取得する
+    /// </summary>
+    /// <returns>当たっているかどうか</returns>
+    public bool GetCollisionFlag()
+    {
+        return m_IsCollision;
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "BreakTower")
+        {
+            m_IsCollision = true;
+        }
+    }
+
+
+}
