2b6d9272db2451e828fa6ec1c1d046169cf5f039
Yano Atsuya <ATYcreateaChiya619@gmail.com>
2017-10-19 22:47:27 +0900

    [矢野]地盤沈下作成

---
+++ GFF04GameProject/Assets/yano/script/Break_Ground.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Break_Ground : MonoBehaviour
+{
+    [SerializeField]
+    [Header("地面の当たり判定")]
+    private GameObject ground_collide_;
+
+    private bool isBreak;
+
+    // Use this for initialization
+    void Start()
+    {
+        isBreak = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "bom")
+        {
+            isBreak = true;
+            Destroy(ground_collide_);
+            Destroy(this.gameObject);
+        }
+    }
+
+    public bool Get_BreakFlag()
+    {
+        return isBreak;
+    }
+}
---
+++ GFF04GameProject/Assets/yano/script/Ground.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject break_ground_obj_;
+    private Break_Ground break_ground_;
+
+    [SerializeField]
+    [Header("沈下後のパーツ")]
+    private GameObject break_ground_parts_;
+
+    [SerializeField]
+    [Header("穴の判定")]
+    private GameObject hole_collide_obj_;
+
+    private bool isClear;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        break_ground_ = break_ground_obj_.GetComponent<Break_Ground>();
+
+        isClear = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (break_ground_.Get_BreakFlag() && !isClear)
+        {
+            Instantiate(break_ground_parts_, transform);
+            Instantiate(hole_collide_obj_, transform);
+
+            isClear = true;
+        }
+    }
+}
