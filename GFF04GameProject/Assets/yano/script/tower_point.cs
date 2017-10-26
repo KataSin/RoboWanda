5ba43530dfd8de6c2cca20ecbf6616e11649f23d
Yano Atsuya <ATYcreateaChiya619@gmail.com>
2017-10-26 17:51:11 +0900

    [矢野]TowerPointタグ追加、ボスが狙う位置追加

---
+++ GFF04GameProject/Assets/yano/script/tower_point.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tower_point : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tower_parent_obj_;
+    private Break tower_break_;
+
+    private bool isBreak;
+
+    // Use this for initialization
+    void Start()
+    {
+        tower_break_ = tower_parent_obj_.GetComponent<Break>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isBreak = tower_break_.Get_BreakFlag();
+    }
+}
