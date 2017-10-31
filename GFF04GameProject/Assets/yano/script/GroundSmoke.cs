f153b56e5dc1ec3bd68699ca0ed6418e68a39cc8
Yano Atsuya <ATYcreateaChiya619@gmail.com>
2017-10-31 10:55:05 +0900

    [矢野]地盤沈下砂煙、ボムの爆破によるボスダメージ処理削除

---
+++ GFF04GameProject/Assets/yano/script/GroundSmoke.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundSmoke : MonoBehaviour
+{
+    private ParticleSystem smoke_;
+
+    // Use this for initialization
+    void Start()
+    {
+        smoke_ = GetComponent<ParticleSystem>();
+        smoke_.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!smoke_.isPlaying)
+            Destroy(this.gameObject);
+    }
+}
