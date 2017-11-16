3dc15543bda0ef203ef4feb6864972ab0f1aec16
Yano Atsuya <ATYcreateaChiya619@gmail.com>
2017-11-16 21:57:07 +0900

    [矢野]テスト配置(ファイル場所:yano/_scene/testHaiti←たぶん)

--- GFF04GameProject/Assets/yano/script/Debris.cs
+++ GFF04GameProject/Assets/yano/script/Debris.cs
@@ -10,9 +10,22 @@ public class Debris : MonoBehaviour
     private Rigidbody rigids_;
     private Vector3 clampVelocities;
 
+    [SerializeField]
+    private float destroyTime;
+
     // Use this for initialization
     void Start()
     {
+    }
+
+    void Update()
+    {
+        destroyTime -= 1.0f * Time.deltaTime;
+
+        if (destroyTime <= 0f)
+        {
+
+        }
     }
 
     // Update is called once per frame
@@ -24,7 +37,7 @@ public class Debris : MonoBehaviour
             clampVelocities = rigids_.velocity;
             clampVelocities.x = Mathf.Clamp(rigids_.velocity.x, -10f, 10f);
             clampVelocities.z = Mathf.Clamp(rigids_.velocity.z, -10f, 10f);
-            clampVelocities.y = Mathf.Clamp(rigids_.velocity.y, rigids_.velocity.y, 50f);
+            clampVelocities.y = Mathf.Clamp(rigids_.velocity.y, rigids_.velocity.y, 10f);
             rigids_.velocity = clampVelocities;
         }
     }
