0f68464a1bdc10a1a7b8808944e5e6c7af18c445
G016A1351\walker <walker418@hotmail.com>
2017-11-17 14:36:47 +0900

    カメラに距離選択を追加（エイム視点は一時廃止）

--- GFF04GameProject/Assets/ho/Player/Scripts/PlayerController.cs
+++ GFF04GameProject/Assets/ho/Player/Scripts/PlayerController.cs
@@ -38,7 +38,7 @@ public class PlayerController : MonoBehaviour
     private float m_BrakePowerDash = 18.0f;         // ダッシュ時のブレーキ速度（メートル/秒/秒）
     [SerializeField]
     private float m_RotateSpeedDash = 450.0f;       // ダッシュ時の回転速度（度/秒）
-    // 爆弾投げ時の移動関連
+    // 照準中の移動関連
     [SerializeField]
     private float m_MaxSpeedAiming = 5.0f;          // 照準中の最高速度（メートル/秒）
     [SerializeField]
@@ -56,8 +56,9 @@ public class PlayerController : MonoBehaviour
     private float m_RotateSpeedCreeping = 45.0f;    // 匍匐時の回転速度（度/秒）
     // 移動処理関連変数
     float m_VelocityY = 0.0f;                       // y軸方向の移動量
-    float m_SpeedX = 0.0f;                          // X軸速度（右はプラス、左はマイナス）
-    float m_SpeedZ = 0.0f;                          // z軸速度（前進はプラス、後退はマイナス）
+    float m_Speed = 0.0f;                           // 通常時の移動速度
+    float m_SpeedX = 0.0f;                          // 照準中のX軸速度（右はプラス、左はマイナス）
+    float m_SpeedZ = 0.0f;                          // 照準中のz軸速度（前進はプラス、後退はマイナス）
     float m_CurrentSpeedLimit;                      // 現在の速度制限
     float m_CurrentBrakePower;                      // 現在のブレーキ速度
     float m_CurrentRotateSpeed;                     // 現在の回転速度
@@ -204,7 +205,7 @@ public class PlayerController : MonoBehaviour
     // 通常時の移動
     void NormalMove()
     {
-        // カメラの正面向きのベクトルを取得
+        /*// カメラの正面向きのベクトルを取得
         Vector3 forward = Camera.main.transform.forward;
         // y成分を無視する
         forward.y = 0;
@@ -282,6 +283,137 @@ public class PlayerController : MonoBehaviour
 
         // 移動処理
         Vector3 velocity = forward * m_SpeedZ + Camera.main.transform.right * m_SpeedX;
+        // 重力加速度を加算
+        m_VelocityY -= m_Gravity * Time.deltaTime;
+        // y軸方向の移動量を加味する
+        velocity.y = m_VelocityY;
+        // キャラクターコントローラーに命令して移動する
+        m_Controller.Move(velocity * Time.deltaTime);
+
+        // 移動方向に向ける
+        Vector3 direction = transform.position - m_PrevPosition;
+        if (direction.sqrMagnitude > 0)
+        {
+            Vector3 orientiation = Vector3.Slerp(transform.forward,
+                new Vector3(direction.x, 0.0f, direction.z),
+                m_CurrentRotateSpeed * Time.deltaTime / Vector3.Angle(transform.forward, direction));
+            transform.LookAt(transform.position + orientiation);
+            m_PrevPosition = transform.position;
+        }
+
+        // アニメーターに命令して、アニメーションを再生する
+        // プレイヤー現在の移動量を取得
+        float current_speed;
+        current_speed = m_Controller.velocity.magnitude;
+        if (current_speed > m_CurrentSpeedLimit) current_speed = m_CurrentSpeedLimit;
+        m_Animator.SetFloat("NormalSpeed", current_speed);*/
+
+        /*// カメラの正面向きのベクトルを取得
+        Vector3 forward = Camera.main.transform.forward;
+        // y成分を無視する
+        forward.y = 0;
+        // 正規化する
+        forward.Normalize();
+
+        // 方向入力を取得
+        float axisHorizontal = Input.GetAxisRaw("Horizontal_L");    // x軸（左右）
+        float axisVertical = Input.GetAxisRaw("Vertical_L");        // z軸（上下）
+
+        // 移動速度、および回転速度の変更
+        if (m_IsDash)
+        {
+            if (m_CurrentSpeedLimit < m_MaxSpeedDash)
+                m_CurrentSpeedLimit += 0.4f;
+            m_CurrentRotateSpeed = m_RotateSpeedDash;
+        }
+        else
+        {
+            if (m_CurrentSpeedLimit > m_MaxSpeed)
+                m_CurrentSpeedLimit -= 0.2f;
+            if (m_CurrentRotateSpeed > m_RotateSpeed)
+                m_CurrentRotateSpeed -= 0.1f;
+        }
+
+        Vector3 velocity = Vector3.zero;
+        velocity = forward * axisVertical * m_CurrentSpeedLimit + Camera.main.transform.right * axisHorizontal * m_CurrentSpeedLimit;
+
+        // 重力加速度を加算
+        m_VelocityY -= m_Gravity * Time.deltaTime;
+        // y軸方向の移動量を加味する
+        velocity.y = m_VelocityY;
+        // キャラクターコントローラーに命令して移動する
+        m_Controller.Move(velocity * Time.deltaTime);
+
+        // 移動方向に向ける
+        Vector3 direction = transform.position - m_PrevPosition;
+        if (direction.sqrMagnitude > 0)
+        {
+            Vector3 orientiation = Vector3.Slerp(transform.forward,
+                new Vector3(direction.x, 0.0f, direction.z),
+                m_CurrentRotateSpeed * Time.deltaTime / Vector3.Angle(transform.forward, direction));
+            transform.LookAt(transform.position + orientiation);
+            m_PrevPosition = transform.position;
+        }
+
+        // アニメーターに命令して、アニメーションを再生する
+        // プレイヤー現在の移動量を取得
+        float current_speed;
+        current_speed = m_Controller.velocity.magnitude;
+        if (current_speed > m_CurrentSpeedLimit) current_speed = m_CurrentSpeedLimit;
+        m_Animator.SetFloat("NormalSpeed", current_speed);
+
+        Debug.Log(m_CurrentSpeedLimit);*/
+
+        // カメラの正面向きのベクトルを取得
+        Vector3 forward = Camera.main.transform.forward;
+        // y成分を無視する
+        forward.y = 0;
+        // 正規化する
+        forward.Normalize();
+
+        // 方向入力を取得
+        float axisHorizontal = Input.GetAxisRaw("Horizontal_L");    // x軸（左右）
+        float axisVertical = Input.GetAxisRaw("Vertical_L");        // z軸（上下）
+
+        // 減速する（入力が無い場合）
+        if (axisHorizontal == 0 && axisVertical == 0)
+        {
+            m_Speed = Mathf.Max(m_Speed - m_CurrentBrakePower * Time.deltaTime, 0);
+        }
+
+        // 接地状態であれば加速可能
+        if (m_Controller.isGrounded)
+        {
+            // 加速する
+            float accel;
+            accel = (m_IsDash) ? accel = m_AccelPowerDash : accel = m_AccelPower;
+            m_Speed += accel * Time.deltaTime;
+
+            // 速度制限、ブレーキ速度、および回転速度の変更
+            if (m_IsDash)
+            {
+                m_CurrentSpeedLimit = m_MaxSpeedDash;
+                m_CurrentBrakePower = m_BrakePowerDash;
+                m_CurrentRotateSpeed = m_RotateSpeedDash;
+            }
+            else
+            {
+                if (m_CurrentSpeedLimit > m_MaxSpeed)
+                {
+                    m_CurrentSpeedLimit -= 0.1f;
+                }
+                m_CurrentBrakePower = m_BrakePower;
+                if (m_CurrentRotateSpeed > m_RotateSpeed)
+                {
+                    m_CurrentRotateSpeed -= 0.1f;
+                }
+            }
+            // 速度を制限する
+            m_Speed = Mathf.Clamp(m_Speed, 0.0f, m_CurrentSpeedLimit);
+        }
+
+        // 移動処理
+        Vector3 velocity = forward * axisVertical * m_Speed + Camera.main.transform.right * axisHorizontal * m_Speed;
         // 重力加速度を加算
         m_VelocityY -= m_Gravity * Time.deltaTime;
         // y軸方向の移動量を加味する
@@ -425,8 +557,8 @@ public class PlayerController : MonoBehaviour
         forward.Normalize();
 
         // 方向入力を取得
-        float axisHorizontal = Input.GetAxisRaw("Horizontal_L");    // x軸
-        float axisVertical = Input.GetAxisRaw("Vertical_L");        // z軸
+        float axisHorizontal = Input.GetAxisRaw("Horizontal_L");    // x軸（左右）
+        float axisVertical = Input.GetAxisRaw("Vertical_L");        // z軸（上下）
 
         // 減速する（入力が無い場合 or 進行方向と逆に入力がある場合）
         // X軸（左右キー）
