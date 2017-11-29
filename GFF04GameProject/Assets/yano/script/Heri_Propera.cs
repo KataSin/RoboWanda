5a51e39d6fad20f2c3786ce611e443a069608056
Yano Atsuya <ATYcreateaChiya619@gmail.com>
2017-11-30 02:57:21 +0900

    [矢野]タイトル途中段階

---
+++ GFF04GameProject/Assets/yano/script/Heri_Propera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heri_Propera : MonoBehaviour
+{
+    [SerializeField]
+    [Header("メインプロペラ")]
+    private GameObject mainPropera_;
+
+    [SerializeField]
+    [Header("サブプロペラ")]
+    private GameObject subPropera_;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mainPropera_.transform.Rotate(Vector3.forward * 15f);
+
+        subPropera_.transform.Rotate(Vector3.right * 20f);
+    }
+}
---
+++ GFF04GameProject/Assets/yano/script/IKSample.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKSample : MonoBehaviour
+{
+
+    private Animator animator_;
+
+    [SerializeField]
+    private GameObject destination_;
+
+    // Use this for initialization
+    void Start()
+    {
+        animator_=GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnAnimatorIK()
+    {
+        animator_.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1f);
+        animator_.SetIKPosition(AvatarIKGoal.LeftHand, destination_.transform.position);
+
+        //animator_.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1f);
+        //animator_.SetIKRotation(AvatarIKGoal.LeftHand, destination_.transform.rotation);
+    }
+}
