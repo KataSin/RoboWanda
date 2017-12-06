0df6b46863d4a2eb618c2dbf9e6aff9f4b35c28d
Yano Atsuya <ATYcreateaChiya619@gmail.com>
2017-12-07 02:53:28 +0900

    [矢野]チュートリアル途中

---
+++ GFF04GameProject/Assets/yano/script/TutorialManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject clear_pod_;
+
+    [SerializeField]
+    private GameObject missionC_ui_;
+
+    [SerializeField]
+    private GameObject mission1_ui_;
+
+    [SerializeField]
+    private GameObject camera_pos_;
+
+    // Use this for initialization
+    void Start()
+    {
+        missionC_ui_.SetActive(false);
+        mission1_ui_.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (camera_pos_.GetComponent<CameraPosition_Tutorial>().GetMode() == 0
+            && camera_pos_.GetComponent<CameraPosition_Tutorial>().Get_T() >= 3f
+            && !clear_pod_.GetComponent<ClearPoint>().GetClear())
+        {
+            mission1_ui_.SetActive(true);
+        }
+
+
+        if (clear_pod_.GetComponent<ClearPoint>().GetClear())
+        {
+            missionC_ui_.SetActive(true);
+            mission1_ui_.SetActive(false);
+        }
+    }
+}
