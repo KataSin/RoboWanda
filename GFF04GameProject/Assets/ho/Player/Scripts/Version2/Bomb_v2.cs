5587ad81c36620bc20a1b8936dc71b001babea18
Yano Atsuya <ATYcreateaChiya619@gmail.com>
2017-12-15 15:53:16 +0900

    [矢野]SE、砂煙微調整

--- GFF04GameProject/Assets/ho/Player/Scripts/Version2/Bomb_v2.cs
+++ GFF04GameProject/Assets/ho/Player/Scripts/Version2/Bomb_v2.cs
@@ -14,14 +14,21 @@ public class Bomb_v2 : MonoBehaviour
     private GameObject m_Explosion;     // 爆発の当たり判定
     Rigidbody m_RigidBody;
 
+    private Quaternion m_currentRotation;
+
+    private bool isLanding;
+
     // Use this for initialization
     void Start()
     {
-        m_RigidBody = GetComponent<Rigidbody>();    
+        m_RigidBody = GetComponent<Rigidbody>();
+
+        isLanding = false;
     }
     // Update is called once per frame
     void Update()
     {
+
         // RBボタンが押されてない間に、LBボタンを押すと起爆
         if (!Input.GetButton("Aim") && Input.GetButtonDown("Bomb_Throw"))
         {
@@ -29,18 +36,35 @@ public class Bomb_v2 : MonoBehaviour
             // 爆発の当たり判定を発生
             Instantiate(m_Explosion, transform.position, Quaternion.identity);
         }
+
+        if (isLanding)
+        {
+            transform.rotation=m_currentRotation;
+            return;
+        }
+
+        Vector3 l_bomForward = GetComponent<Rigidbody>().velocity;
+        transform.rotation = Quaternion.LookRotation(l_bomForward) * Quaternion.Euler(90, 0, 0);
+
+
     }
 
     // 接触判定
     public void OnTriggerEnter(Collider other)
     {
         // 他の爆弾とプレイヤーとの接触判定は発生しない
-        if (other.tag == "Bomb" || other.tag == "Player"||other.tag=="SandSmoke") return;
+        if (other.tag == "Bomb" || other.tag == "Player" || other.tag == "SandSmoke") return;
 
 
         //矢野追加10270231
-        transform.parent = other.transform;
-        m_RigidBody.velocity = Vector3.zero;
-        m_RigidBody.isKinematic = true;
+        if (!isLanding)
+        {
+            m_currentRotation = transform.rotation;
+            transform.parent = other.transform;
+            m_RigidBody.velocity = Vector3.zero;
+            m_RigidBody.isKinematic = true;
+
+            isLanding = true;
+        }
     }
 }
