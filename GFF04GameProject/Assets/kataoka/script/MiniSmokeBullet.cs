4d68c67ebd319803518cd992b05f49d374bb92f8
kataoka <ktok223@gmail.com>
2018-01-12 10:04:57 +0900

    スモークボム追加

---
+++ GFF04GameProject/Assets/kataoka/script/MiniSmokeBullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniSmokeBullet : MonoBehaviour
+{
+    public float m_lifeTime;
+    private float m_Time;
+    // Use this for initialization
+    void Start()
+    {
+        m_Time = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Time += Time.deltaTime;
+        if (m_Time >= m_lifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
